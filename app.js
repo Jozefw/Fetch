6098981bba4097d82d1a7976898adb393606007c
Jozef <jozefw@sbcglobal.net>
2018-01-18 13:53:13 -0800

    final commit for es6 promises and arrow functions

--- app.js
+++ app.js
@@ -1,75 +1,53 @@
 
+document.getElementById('button1').addEventListener('click',getText);
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click',getApi);
+
+// ************** Get Local text file ************
+function getText(e){
+  fetch('test.txt')
+  .then((res)=>{
+    return res.text();
+  })
+  .then((data)=>{
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch((err)=>{
+  });
+}
+
+// ************** Get Local JSON file ************
+function getJson(){
+  fetch('posts.json')
+  .then((res)=>{
+    return res.json();
+  })
+  .then((data)=>{
+    let postOutput ='';
+    data.forEach((post)=>{
+      postOutput = postOutput + `
+      <li>${post.title} : ${post.body}</li>`
+    });
+    document.getElementById('output').innerHTML = postOutput;
+  })
+}
+// ************** Get API Data ************
+
+function getApi(){
+  fetch('https://api.github.com/users')
+  .then((res)=>{
+    return res.json();
+  })
+  .then((data)=>{
+    let userOutput ='';
+    data.forEach((user)=>{
+      userOutput = userOutput + `
+      <li>${user.login}</li>`
+    });
+    document.getElementById('output').innerHTML = userOutput;
+  })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// document.getElementById('button1').addEventListener('click', getText);
-
-// document.getElementById('button2').addEventListener('click', getJson);
-
-// document.getElementById('button3').addEventListener('click', getExternal);
-
-// // Get local text file data
-// function getText() {
-//   fetch('test.txt')
-//     .then(function(res){
-//       return res.text();
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//       document.getElementById('output').innerHTML = data;
-//     })
-//     .catch(function(err){
-//       console.log(err);
-//     });
-// }
-
-
-// // Get local json data
-// function getJson() {
-//   fetch('posts.json')
-//     .then(function(res){
-//       return res.json();
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//       let output = '';
-//       data.forEach(function(post) {
-//         output += `<li>${post.title}</li>`;
-//       });
-//       document.getElementById('output').innerHTML = output;
-//     })
-//     .catch(function(err){
-//       console.log(err);
-//     });
-// }
-
-
-// // Get from external API
-// function getExternal() {
-//   fetch('https://api.github.com/users')
-//     .then(function(res){
-//       return res.json();
-//     })
-//     .then(function(data) {
-//       console.log(data);
-//       let output = '';
-//       data.forEach(function(user) {
-//         output += `<li>${user.login}</li>`;
-//       });
-//       document.getElementById('output').innerHTML = output;
-//     })
-//     .catch(function(err){
-//       console.log(err);
-//     });
-// }
+}
